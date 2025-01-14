17aef16d2f8285be73dd6d4f003a133708772938
CharlesDFranca <josecharlestill@gmail.com>
2025-01-14 19:04:53 -0300

    create FindAllPatientsUseCases

---
+++ src/domain/modules/users/use-cases/FindAllPatientsUseCase.ts
@@ -0,0 +1,17 @@
+import { NotFoundError } from "../../../common/errors/NotFoundError";
+import { Patient } from "../entities/Patient";
+import { IPatientRepository } from "../repositories/IPatientRepository";
+
+export class FindAllPatientsUseCases {
+  constructor(private readonly patientRepository: IPatientRepository) {}
+
+  async execute(): Promise<Patient[] | null> {
+    const patients = await this.patientRepository.findAll();
+
+    if (!patients) {
+      throw new NotFoundError("Not patient found");
+    }
+
+    return patients;
+  }
+}
