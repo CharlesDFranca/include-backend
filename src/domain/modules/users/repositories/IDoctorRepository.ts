798f325884398aad73f71b1bf6f045b2b5d95c61
CharlesDFranca <josecharlestill@gmail.com>
2025-01-14 21:17:23 -0300

    create IDoctorRepository

---
+++ src/domain/modules/users/repositories/IDoctorRepository.ts
@@ -0,0 +1,10 @@
+import { CreateDoctorDTO } from "../dtos/doctors/CreateDoctorDTO";
+import { Doctor } from "../entities/Doctor";
+
+export interface IDoctorRepository {
+  create(doctorData: CreateDoctorDTO): Promise<Doctor>;
+  findByID(id: string): Promise<Doctor | null>;
+  findByEmail(email: string): Promise<Doctor | null>;
+  findAll(): Promise<Doctor[] | null>;
+  delete(id: string): Promise<boolean>;
+}
