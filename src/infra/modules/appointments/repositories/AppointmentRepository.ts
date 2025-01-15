643ee7ae3cb4c5529a078a6f2c226daffe9b620a
CharlesDFranca <josecharlestill@gmail.com>
2025-01-15 20:30:24 -0300

    create AppointmentRepository

---
+++ src/infra/modules/appointments/repositories/AppointmentRepository.ts
@@ -0,0 +1,72 @@
+import {
+  AppointmentProps,
+  Appointment,
+} from "../../../../domain/modules/appointments/entities/Appointment";
+import { IAppointmentRepository } from "../../../../domain/modules/appointments/repositories/IAppointmentsRepositories.";
+import { AppointmentModel } from "../models/AppointmentModel";
+
+export class AppointmentRepository implements IAppointmentRepository {
+  async create({ patientID, doctorID, endsAt, startsAt }: AppointmentProps): Promise<Appointment> {
+    const appointmentDocument = await AppointmentModel.create({
+      doctorID,
+      patientID,
+      endsAt,
+      startsAt,
+    });
+
+    await appointmentDocument.save();
+
+    const appointment = new Appointment(
+      appointmentDocument._id.toString(),
+      appointmentDocument.patientID.toString(),
+      appointmentDocument.doctorID.toString(),
+      appointmentDocument.startsAt,
+      appointmentDocument.endsAt,
+    );
+
+    return appointment;
+  }
+
+  async findByID(id: string): Promise<Appointment | null> {
+    const appointmentData = await AppointmentModel.findById(id);
+
+    if (!appointmentData) {
+      return null;
+    }
+
+    const appointment = new Appointment(
+      appointmentData._id.toString(),
+      appointmentData.patientID.toString(),
+      appointmentData.doctorID.toString(),
+      appointmentData.startsAt,
+      appointmentData.endsAt,
+    );
+
+    return appointment;
+  }
+
+  async findAll(): Promise<Appointment[] | null> {
+    const appointmentsData = await AppointmentModel.find();
+
+    if (!appointmentsData) {
+      return null;
+    }
+
+    const appointments = appointmentsData.map((appointment) => {
+      return new Appointment(
+        appointment._id.toString(),
+        appointment.patientID.toString(),
+        appointment.doctorID.toString(),
+        appointment.startsAt,
+        appointment.endsAt,
+      );
+    });
+
+    console.log(appointments);
+
+    return appointments;
+  }
+  async delete(id: string): Promise<void> {
+    await AppointmentModel.deleteOne({ _id: id });
+  }
+}
