af522022534b6e4b2031064d3a3f78b4b77f81ac
CharlesDFranca <josecharlestill@gmail.com>
2025-01-14 21:32:37 -0300

    create DoctorModel

---
+++ src/infra/modules/users/models/DoctorModel.ts
@@ -0,0 +1,27 @@
+import { Schema, model } from "mongoose";
+
+const TimeIntervalSchema = new Schema({
+  start: { type: String, required: true },
+  end: { type: String, required: true },
+});
+
+const AvailabilitySchema = new Schema({
+  monday: [TimeIntervalSchema],
+  tuesday: [TimeIntervalSchema],
+  wednesday: [TimeIntervalSchema],
+  thursday: [TimeIntervalSchema],
+  friday: [TimeIntervalSchema],
+  saturday: [TimeIntervalSchema],
+  sunday: [TimeIntervalSchema],
+});
+
+const DoctorSchema = new Schema({
+  id: { type: String, required: false },
+  name: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  specialty: { type: String, required: true },
+  availability: { type: AvailabilitySchema, required: true },
+});
+
+export const DoctorModel = model("Doctor", DoctorSchema);
