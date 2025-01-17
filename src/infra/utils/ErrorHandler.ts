f1f7c385ab544a27a6c07949ff241ad2e9b73ac7
CharlesDFranca <josecharlestill@gmail.com>
2025-01-16 23:29:36 -0300

    add new errors classes at errorHandler

--- src/infra/utils/ErrorHandler.ts
+++ src/infra/utils/ErrorHandler.ts
@@ -1,6 +1,9 @@
 import { Request, Response, NextFunction, ErrorRequestHandler } from "express";
 import { MissingRequiredFieldsError } from "../../domain/common/errors/MissingRequiredFieldsError";
 import { NotFoundError } from "../../domain/common/errors/NotFoundError";
+import { BadRequestError } from "../../domain/common/errors/BadRequestError";
+import { UnauthorizedError } from "../../domain/common/errors/UnauthorizedError";
+import { InvalidCredentialsError } from "../../domain/common/errors/InvalidCredentialsError";
 
 export const errorHandler: ErrorRequestHandler = (
   err,
@@ -18,6 +21,27 @@ export const errorHandler: ErrorRequestHandler = (
 
   if (err instanceof NotFoundError) {
     res.status(404).json({
+      error: err.message || "Not Found",
+    });
+    return;
+  }
+
+  if (err instanceof BadRequestError) {
+    res.status(400).json({
+      error: err.message || "Not Found",
+    });
+    return;
+  }
+
+  if (err instanceof UnauthorizedError) {
+    res.status(401).json({
+      error: err.message || "Not Found",
+    });
+    return;
+  }
+
+  if (err instanceof InvalidCredentialsError) {
+    res.status(400).json({
       error: err.message || "Not Found",
     });
     return;
