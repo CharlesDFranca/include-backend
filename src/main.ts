d2002d5a679913742d2d40f760bf08fd2ffcd04f
CharlesDFranca <josecharlestill@gmail.com>
2025-01-17 23:42:06 -0300

    config cors

--- src/main.ts
+++ src/main.ts
@@ -12,7 +12,13 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
-app.use(cors());
+app.use(
+  cors({
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  }),
+);
 
 app.use(doctorRoutes);
 app.use(patientRoutes);
