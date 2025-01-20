a74ced12f749866cc95e1130ace18cb205b1d95b
CharlesDFranca <josecharlestill@gmail.com>
2025-01-19 23:25:06 -0300

    check if has overlapping appoinment

--- src/domain/modules/appointments/use-cases/CreateAppointmentUseCase.ts
+++ src/domain/modules/appointments/use-cases/CreateAppointmentUseCase.ts
@@ -1,13 +1,17 @@
+import { InvalidDataError } from "../../../common/errors/InvalidDataError";
 import { NotFoundError } from "../../../common/errors/NotFoundError";
 import { IDoctorRepository } from "../../users/repositories/IDoctorRepository";
 import { IPatientRepository } from "../../users/repositories/IPatientRepository";
+import { Appointment } from "../entities/Appointment";
 import { IAppointmentRepository } from "../repositories/IAppointmentsRepositories.";
 
 export type CreateAppointmentInput = {
   patientID: string;
   doctorID: string;
-  startsAt: Date;
+  startsAt: string;
 };
+
+const DaysOfWeek = ["sunday", "monday", "tuesday", "wednesday", "thursday", "friday", "saturday"];
 
 export class CreateAppointmentUseCase {
   constructor(
@@ -17,9 +21,9 @@ export class CreateAppointmentUseCase {
   ) {}
 
   async execute({ doctorID, patientID, startsAt }: CreateAppointmentInput) {
-    const pacient = await this.patientRepository.findByID(patientID);
+    const patient = await this.patientRepository.findByID(patientID);
 
-    if (!pacient) {
+    if (!patient) {
       throw new NotFoundError("Patient not found");
     }
 
@@ -29,17 +33,46 @@ export class CreateAppointmentUseCase {
       throw new NotFoundError("Doctor not found");
     }
 
+    const patientAppointments =
+      await this.appointmentRepository.findAppointmentsByPatient(patientID);
+
+    if (patientAppointments) {
+      patient.setAppointment = patientAppointments;
+    }
+
+    const doctorAppointments = await this.appointmentRepository.findAppointmentsByDoctor(doctorID);
+
+    if (doctorAppointments) {
+      doctor.setAppointment = doctorAppointments;
+    }
+
+    const startAppointment = new Date(startsAt);
+
+    const hours = startAppointment.getHours().toString().padStart(2, "0");
+    const minutes = startAppointment.getMinutes().toString().padStart(2, "0");
+
+    const formattedTime = `${hours}:${minutes}`;
+
+    if (!doctor.getAvailability.isAvailable(DaysOfWeek[startAppointment.getDay()], formattedTime)) {
+      throw new InvalidDataError(
+        `It is not possible to make an appointment at this time: ${DaysOfWeek[startAppointment.getDay()]} at ${formattedTime}`,
+      );
+    }
+
     const endsAt = new Date(new Date(startsAt).getTime() + 30 * 60 * 1000);
 
-    const appointment = await this.appointmentRepository.create({
+    Appointment.validateOverlappingAppointments(patient.getAppointments, startAppointment, endsAt);
+    Appointment.validateOverlappingAppointments(doctor.getAppointments, startAppointment, endsAt);
+
+    const newAppointment = await this.appointmentRepository.create({
       doctorID,
       patientID,
-      startsAt,
+      startsAt: startAppointment,
       endsAt,
     });
 
-    appointment.addAppointment(pacient, doctor);
+    newAppointment.addAppointment(patient, doctor);
 
-    return appointment;
+    return newAppointment;
   }
 }
