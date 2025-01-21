2c26f6dabcef63a726084af531114ac738eb3aa4
CharlesDFranca <josecharlestill@gmail.com>
2025-01-20 21:43:20 -0300

    replace Error to InvalidDataError

--- src/domain/modules/users/value-objects/Email.ts
+++ src/domain/modules/users/value-objects/Email.ts
@@ -1,25 +1,25 @@
+import { InvalidDataError } from "../../../common/errors/InvalidDataError";
+
 export class Email {
-    constructor(private readonly value: string) {
-      const EMAILREGEX = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
-  
-      this.value = value.trim().toLowerCase();
-  
-      if (!EMAILREGEX.test(this.value)) {
-        console.log(value);
-        throw new Error("Invalid email");
-      }
-    }
-  
-    get getEmail(): string {
-      return this.value;
-    }
-  
-    public updateEmail(email: string): Email {
-      return new Email(email);
-    }
-  
-    public toString(): string {
-      return this.getEmail;
+  constructor(private readonly value: string) {
+    const EMAILREGEX = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+
+    this.value = value.trim().toLowerCase();
+
+    if (!EMAILREGEX.test(this.value)) {
+      throw new InvalidDataError("Invalid email");
     }
   }
-  
+
+  get getEmail(): string {
+    return this.value;
+  }
+
+  public updateEmail(email: string): Email {
+    return new Email(email);
+  }
+
+  public toString(): string {
+    return this.getEmail;
+  }
+}
